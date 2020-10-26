75411253793b2427020280cd38b76784603f6133
Weslley Braga <wesbragagt@gmail.com>
2020-10-26 00:07:21 -0500

    added saved page

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,16 @@
-import React, { useState } from 'react'
-import { useIndeed } from './hooks'
-import {Card} from './components/Card'
-import { Loading } from './components/Loading'
+import React from 'react'
+import Home from './pages/Home'
+import Saved from './pages/Saved'
+import { Route, Switch } from 'react-router-dom';
 
-function App () {
-  const [position, setPosition] = useState('')
-  const [location, setLocation] = useState('')
-  const [getJobsFromIndeed, { data, loading }] = useIndeed()
-
-  function handleSubmit (e) {
-    e.preventDefault()
-    const { position, location } = e.target
-    getJobsFromIndeed({ position: position.value, location: location.value })
-  }
-
+function App() {
   return (
-    <div className='container mx-auto'>
-      <form className='w-full max-w-sm mx-auto mb-2' onSubmit={handleSubmit}>
-        <div className='flex items-center border-b border-teal-500 py-2'>
-          <input
-            placeholder='position'
-            className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none'
-            id='position'
-            name='position'
-            type='text'
-            onChange={e => setPosition(e.target.value)}
-            value={position}
-          />
-          <input
-            placeholder='location'
-            className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none'
-            id='location'
-            name='location'
-            type='text'
-            onChange={e => setLocation(e.target.value)}
-            value={location}
-          />
-          <button
-            className='flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded'
-            type='submit'
-          >
-            Indeed
-          </button>
-        </div>
-      </form>
-      <div className='flex flex-wrap mb-4'>
-        {
-          loading && <Loading/>
-        }
-        {
-          data && data.length > 0 && data.map((job, i) => (
-            <Card key={`${i}-${job.job}`} data={job}/>
-          ))
-        }
-      </div>
-    </div>
+      <main>
+          <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/saved" component={Saved} />
+          </Switch>
+      </main>
   )
 }
 
