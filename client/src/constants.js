ebed400df6c842cda5a617ec7f0022438457ef26
Weslley Braga <wesbragagt@gmail.com>
2021-01-18 20:02:20 -0600

    fixed client issue with pulling jobs

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : 'http://localhost:5000/dev'
+export const API_URL = process.env.REACT_APP_API_URL
 export const API_KEY = process.env.REACT_APP_API_KEY
